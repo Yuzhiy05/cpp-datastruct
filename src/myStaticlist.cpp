545a94cc6d0985f546f2693fbc35d846f4673cb7
yuzhiy05 <ImoutoCon1999@outlook.com>
2022-08-25 23:52:54 +0800

    add mystaticklist.cpp

---
+++ src/myStaticlist.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+#define ElemType int
+#define Maxsize 50 //线性表最大长度
+typedef struct{
+	int data[Maxsize];
+	unsigned int length;
+}Sqlist;
+
+#define InitSize 100
+typedef struct 
+{ 
+	ElemType *data;
+	unsigned int Maxsize,length; 
+	
+}SeqList;
+
+
+ 
+
